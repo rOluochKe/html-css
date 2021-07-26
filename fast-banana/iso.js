3f21b70ed387fbaf8f6e9db7c04e7bf16c900915
Raymond Oluoch <rasholuoch@gmail.com>
2021-07-26 14:33:06 +0300

    adding complete fast banana project

--- fast-banana/iso.js
+++ fast-banana/iso.js
@@ -2,16 +2,18 @@ $(document).ready(function () {
   // init Isotope
   let $grid = $('.picture-container').isotope({
     // options
-  });
+  })
+
   // filter items on button click
   $('.filter-button-group').on('click', 'button', function () {
-    let filterValue = $(this).attr('data-filter');
+    let filterValue = $(this).attr('data-filter')
     $grid.isotope({
-      filter: filterValue
-    });
-  });
+      filter: filterValue,
+    })
+  })
+
   //images loaded
   $grid.imagesLoaded().progress(function () {
-    $grid.isotope('layout');
-  });
-});
+    $grid.isotope('layout')
+  })
+})
